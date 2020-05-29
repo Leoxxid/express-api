7ac452bbc8b1c68091681375b54ac95732ed91f1
leoxxid <leoxxid@hotmail.com>
2020-05-29 03:50:38 -0300

    :sparkles: Creation of first Model

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
 // Iniciando o app
 const app = express();
 
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+requireDir("./src/models");
+
+const Product = mongoose.model('Product')
 
 app.get('/', (req, res) =>{
-  res.send('Hellow Leonardo')
+  Product.create({
+    title: 'API', 
+    description: 'Build first express api', 
+    url: 'github.com',
+  })
+  res.send('Hellow Leonardoo')
 });
-
 
 app.listen(3000);
